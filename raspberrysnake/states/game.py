da4b6d5c7228b07090edae7497b4fe15e2ce81ca
CraicOverflow89 <craic.overflow@hotmail.com>
2019-11-13 16:12:41 +0000

    Rendering basic entities in StateGame class.

--- raspberrysnake/states/game.py
+++ raspberrysnake/states/game.py
@@ -1,12 +1,16 @@
 from core.states import State
+from entities.snake import Snake
+from library.dimensions import Dimensions
 from library.point import Point
 
 class StateGame(State):
 
 	def __init__(self, app):
 		super().__init__(app, "GAME")
+		self.world_pos = Point(32, 48)
+		self.world_size = Dimensions(576, 416)
 		self.score = 0
-		self.snake = Snake()
+		self.snake = Snake(Point(5, 3))
 		self.fruit = []
 
 	def onKeyPressed(self, event):
@@ -16,23 +20,25 @@ class StateGame(State):
 
 	def render(self, gfx):
 
-		# Render Snake
-		self.render_snake(gfx)
-
-		# Render Fruit
-		self.render_fruit(gfx)
-
 		# Render Score
-		self.render_score(gfx)
-
-	def render_fruit(self, gfx):
-		pass
-
-	def render_score(self, gfx):
 		gfx.draw_text("Score %s" % self.score, Point(10, 10))
 
-	def render_snake(self, gfx):
-		pass
+		# Render Entities
+		self.render_entities(gfx.offset_graphics(self.world_pos))
+
+		# Render Border
+		gfx.draw_rect(self.world_pos, self.world_size, "white", False)
+
+	def render_entities(self, gfx):
+
+		# Render Snake
+		self.snake.render(gfx)
+
+		# Render Fruit
+		gfx.draw_rect(Point(32 * 2, 32 * 7), Dimensions(32, 32), "red", True)
+		gfx.draw_rect(Point(32 * 9, 32 * 5), Dimensions(32, 32), "red", True)
 
 	def tick(self):
-		pass
+
+		# Snake Movement
+		self.snake.move()
