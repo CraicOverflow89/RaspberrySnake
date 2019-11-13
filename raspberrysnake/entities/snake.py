ec31ad384f83e8fcf7407cf981ce34f42d7636f5
CraicOverflow89 <craic.overflow@hotmail.com>
2019-11-13 15:14:10 +0000

    Created the Snake entity class.

---
+++ raspberrysnake/entities/snake.py
@@ -0,0 +1,14 @@
+class Snake(Entity):
+
+	def __init__(position):
+		super().__init__(position)
+		#self.direction = NORTH
+
+	def grow():
+		# NOTE: tail gets longer due to fruit being eaten
+
+	def move():
+		# NOTE: update position based on direction
+
+	def render(gfx):
+		pass
--- raspberrysnake/states/game.py
+++ raspberrysnake/states/game.py
@@ -1,10 +1,13 @@
 from core.states import State
-from graphics.point import Point
+from library.point import Point
 
 class StateGame(State):
 
 	def __init__(self, app):
 		super().__init__(app, "GAME")
+		self.score = 0
+		self.snake = Snake()
+		self.fruit = []
 
 	def onKeyPressed(self, event):
 
@@ -13,8 +16,23 @@ class StateGame(State):
 
 	def render(self, gfx):
 
-		# TEMP
-		gfx.draw_text("GAME STATE", Point(10, 10))
+		# Render Snake
+		self.render_snake(gfx)
+
+		# Render Fruit
+		self.render_fruit(gfx)
+
+		# Render Score
+		self.render_score(gfx)
+
+	def render_fruit(self, gfx):
+		pass
+
+	def render_score(self, gfx):
+		gfx.draw_text("Score %s" % self.score, Point(10, 10))
+
+	def render_snake(self, gfx):
+		pass
 
 	def tick(self):
 		pass
