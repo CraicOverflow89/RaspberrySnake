da9c011e0c36d120f0b5036e2e61b2f25bbaa5a9
CraicOverflow89 <craic.overflow@hotmail.com>
2019-11-13 20:01:22 +0000

    Overloaded the minus, multiply, divide, equals and not equals operators for the Point class.

--- raspberrysnake/library/point.py
+++ raspberrysnake/library/point.py
@@ -4,5 +4,20 @@ class Point:
 		self.x = x
 		self.y = y
 
-	def __add__(self, point):
-		return Point(self.x + point.x, self.y + point.y)
+	def __add__(self, other):
+		return Point(self.x + other.x, self.y + other.y)
+
+	def __truediv__(self, other):
+		return Point(self.x / other.x, self.y / other.y)
+
+	def __eq__(self, other):
+		return self.x == other.x and self.y == other.y
+
+	def __mul__(self, other):
+		return Point(self.x * other.x, self.y * other.y)
+
+	def __neq__(self, other):
+		return self.x != other.x or self.y != other.y
+
+	def __sub__(self, other):
+		return Point(self.x - other.x, self.y - other.y)
