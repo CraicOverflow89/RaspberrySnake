873049b5bac82f61f95f7c0a82d6d06f4b6e62bd
CraicOverflow89 <craic.overflow@hotmail.com>
2019-11-13 16:08:22 +0000

    Entities have size (as Dimensions).

--- raspberrysnake/entities/entity.py
+++ raspberrysnake/entities/entity.py
@@ -2,8 +2,9 @@ from abc import ABC, abstractmethod
 
 class Entity(ABC):
 
-	def __init__(self, position):
+	def __init__(self, position, size):
 		self.position = position
+		self.size = size
 
 	@abstractmethod
 	def render(self, gfx):
--- raspberrysnake/entities/snake.py
+++ raspberrysnake/entities/snake.py
@@ -1,14 +1,22 @@
+from entities.entity import Entity
+from library.dimensions import Dimensions
+from library.point import Point
+
 class Snake(Entity):
 
-	def __init__(position):
-		super().__init__(position)
+	def __init__(self, position):
+		super().__init__(position, Dimensions(32, 32))
 		#self.direction = NORTH
 
-	def grow():
+	def face(self, direction):
+		self.direction = direction
+
+	def grow(self):
+		pass
 		# NOTE: tail gets longer due to fruit being eaten
 
-	def move():
-		# NOTE: update position based on direction
-
-	def render(gfx):
+	def move(self):
 		pass
+
+	def render(self, gfx):
+		gfx.draw_rect(Point(self.position.x * self.size.width, self.position.y * self.size.height), self.size, "blue", True)
