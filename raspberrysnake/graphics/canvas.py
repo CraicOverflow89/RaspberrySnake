5ee69de1841b58915525e4e0a879eb5de94960cb
CraicOverflow89 <craic.overflow@hotmail.com>
2019-11-13 13:46:01 +0000

    Created the Graphics class.

---
+++ raspberrysnake/graphics/canvas.py
@@ -0,0 +1,15 @@
+from tkinter import *
+
+class Graphics:
+
+	def __init__(self, canvas):
+		self.canvas = canvas
+
+	def draw_image(self, image, position):
+		self.canvas.create_image(position.x, position.y, image = image, anchor = NW)
+
+	def draw_rect(self, position, size, fill):
+		self.canvas.create_rectangle(position.x, position.y, size.width, size.height, fill = fill)
+
+	def draw_text(self, text, position):
+		self.canvas.create_text(position.x, position.y, text = text, font = "Inconsolata 14", fill = "white", anchor = NW)
