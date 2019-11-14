46de2d16977fbdf69ab5a81d3893c11c68c688ce
CraicOverflow89 <craic.overflow@hotmail.com>
2019-11-14 08:23:28 +0000

    Added basic operators to the Dimensions class to add/subtract/multiply/divide (by) integer.

--- raspberrysnake/library/dimensions.py
+++ raspberrysnake/library/dimensions.py
@@ -3,3 +3,15 @@ class Dimensions:
 	def __init__(self, width, height):
 		self.width = width
 		self.height = height
+
+	def __add__(self, value):
+		return Dimensions(self.width + value, self.height + value)
+
+	def __mul__(self, value):
+		return Dimensions(self.width * value, self.height * value)
+
+	def __sub__(self, value):
+		return Dimensions(self.width - value, self.height - value)
+
+	def __truediv__(self, value):
+		return Dimensions(self.width / value, self.height / value)
