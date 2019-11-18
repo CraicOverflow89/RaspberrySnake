d69c3d4d7a6b08ed13ef849ffe4c95bdb2e3f2e8
CraicOverflow89 <craic.overflow@hotmail.com>
2019-11-18 12:18:11 +0000

    Updated the SoundLoader.play method - no longer waits for sound to finish playing.

--- raspberrysnake/audio/sounds.py
+++ raspberrysnake/audio/sounds.py
@@ -1,6 +1,14 @@
+from library.list import ArrayList
+from threading import Thread
 from playsound import playsound
 
 class SoundLoader:
 
 	def play(sound):
-		playsound("resources/sounds/%s.mp3" % sound)
+
+		# Thread Logic
+		def execute(sound, _):
+			playsound("resources/sounds/%s.mp3" % sound)
+
+		# Spawn Thread
+		Thread(target = execute, args = (sound, None), daemon = False).start()
