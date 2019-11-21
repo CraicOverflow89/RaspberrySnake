731101f6ee0245963760c29b6843aa43cf8cb7ae
CraicOverflow89 <craic.overflow@hotmail.com>
2019-11-21 18:27:51 +0000

    WIP: Extending the riem.core.Application class is currently broken.

--- raspberrysnake/app.py
+++ raspberrysnake/app.py
@@ -1,4 +1,22 @@
-from core.application import Application
+from riem.core import Application
+
+# Game Class
+class RaspberrySnake(Application):
+
+	# Constants
+	highscore = 0
+
+	def __init__(self):
+		super().__init__(self, "Raspberry Snake", "StateIntro", "raspberrysnake/states")
+
+	def get_score(self):
+		return Application.highscore
+
+	def new_score(self, value):
+		if value > Application.highscore:
+			Application.highscore = value
+			return True
+		return False
 
 # Launch Application
-Application("INTRO")
+RaspberrySnake()
