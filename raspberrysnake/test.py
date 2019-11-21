04ba309cd617aa763cec723ac3b9d4770f0f6f01
CraicOverflow89 <craic.overflow@hotmail.com>
2019-11-21 14:38:09 +0000

    Temporary test script should now be able to create absolute path to states directory from cwd (even when script resides elsewhere).

--- raspberrysnake/test.py
+++ raspberrysnake/test.py
@@ -18,16 +18,19 @@ def load(directory):
 		# Return States
 		return result.filter(lambda it: issubclass(it, State))
 
+	# Directory Path
+	directory_path = os.path.join(os.getcwd(), directory)
+
 	# List Files
-	directory_path = os.path.join(os.path.dirname(__file__), directory)
-	# NOTE: need to test this when called from another project
 	file_list = ArrayList(os.listdir(directory_path)).reject(lambda it: it.startswith("_")).map(lambda it: it.split(".")[0])
+	# NOTE: current reject is not going to ignore directories
 
 	# Return States
-	return ArrayList(file_list.map(lambda it: load(importlib.import_module("%s.%s" % (directory, it)))))
+	return ArrayList(file_list.map(lambda it: load(importlib.import_module("%s.%s" % (directory.split("/")[-1], it)))))
 	# NOTE: construcrtor/add_all should be creating ArrayList<State>
 	#       but currently getting ArrayList<ArrayList<State>>
 	#       wrapping in list() doesn't work and shouldn't have to do that
 	#       check that arrays and ArrayList objects with only one element are still unpacked correctly
 
-print(load("states"))
+print(load("raspberrysnake/states"))
+# NOTE: would be better to not have to include subdirectory in project
