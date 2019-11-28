cf6e408f27d8d5ec2fcadcc5d85ab6d9873da773
CraicOverflow89 <craic.overflow@hotmail.com>
2019-11-28 11:32:33 +0000

    Method annotations for app.

--- raspberrysnake/app.py
+++ raspberrysnake/app.py
@@ -3,14 +3,14 @@ from riem.core import Application
 # Game Class
 class RaspberrySnake(Application):
 
-	def __init__(self):
-		self.highscore = 0
+	def __init__(self) -> None:
+		self.highscore: int = 0
 		super().__init__("Raspberry Snake", "StateTitle", "raspberrysnake/states", {"colour": "white", "font": "Inconsolata 14"}, "icon")
 
-	def get_score(self):
+	def get_score(self) -> int:
 		return self.highscore
 
-	def new_score(self, value):
+	def new_score(self, value: int) -> bool:
 		if value > self.highscore:
 			self.highscore = value
 			return True
