d7ab23cb70a403e3fc80baf586b5fc74b33bf32a
CraicOverflow89 <craic.overflow@hotmail.com>
2019-11-28 10:15:55 +0000

    Now using riem 0.0.4. Specifying default text options.

--- raspberrysnake/app.py
+++ raspberrysnake/app.py
@@ -5,7 +5,7 @@ class RaspberrySnake(Application):
 
 	def __init__(self):
 		self.highscore = 0
-		super().__init__("Raspberry Snake", "StateTitle", "raspberrysnake/states")
+		super().__init__("Raspberry Snake", "StateTitle", "raspberrysnake/states", {"colour": "white", "font": "Inconsolata 14"})
 
 	def get_score(self):
 		return self.highscore
